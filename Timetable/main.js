485cc976fb76b5b809fa24f72905bb1ee2f66d0d
jakey1610 <mortimer907@gmail.com>
2014-11-08 17:35:59 +0000

    Edits to the Timetable

---
+++ Timetable/main.js
@@ -0,0 +1,15 @@
+function dotw(){
+  var day1 = "Monday";
+  var day2 = "Tuesday";
+  var day3 = "Wednesday";
+  var day4 = "Thursday";
+  var day5 = "Friday";
+  var day6 = "Saturday";
+  var day7 = "Sunday";
+
+  var d = new Date();
+  var n = d.getDay();
+  document.write(dayn)
+}
+
+dotw();
