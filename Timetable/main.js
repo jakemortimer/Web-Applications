1a92e31ea278c228cae34f877203b87de10f6767
jakey1610 <mortimer907@gmail.com>
2014-11-08 19:38:56 +0000

    Couple of Changes
    Still not working or up to scratch but getting there

--- Timetable/main.js
+++ Timetable/main.js
@@ -9,8 +9,23 @@ function dotw(){
   weekday[5] = "Friday";
   weekday[6] = "Saturday";
 
-  var n = weekday[d.getDay()];
-  document.write(n);
+
+  var user = new Array(100);
+  user[1] = "Jake Mortimer";
+  document.getElementById("dotw").innerHTML = "Who is this?";
+  document.getElementById("timetable").innerHTML = "mmhmm.. this guy looks suspicious.";
+  var name = document.getElementById("b4").innerHTML
+  if name == "Jake Mortimer"{
+
+  }
+
+
+
+  var day = weekday[d.getDay()];
+  document.getElementById("dotw").innerHTML = day;
+  var timetable = ["Computing, Computing, Computing, Computing, Computing","Maths, Maths, Maths, Maths, Maths", "English, English, English, English, English", "History, History, History, History, History", "French, French, French, French, French", "You're not at school today.. or at least you shouldn't be...", "You're not at school today.. or at least you shouldn't be..."];
+  var timetable = timetable[d.getDay()];
+  document.getElementById("timetable").innerHTML = timetable;
 }
 
 dotw();
