6087d29c770f75bb1f7e1851b316c19d10c7006b
jakey1610 <mortimer907@gmail.com>
2014-11-08 18:00:23 +0000

    Edits 2
    This is just an edit to the timetable application.

--- Timetable/main.js
+++ Timetable/main.js
@@ -1,15 +1,16 @@
 function dotw(){
-  var day1 = "Monday";
-  var day2 = "Tuesday";
-  var day3 = "Wednesday";
-  var day4 = "Thursday";
-  var day5 = "Friday";
-  var day6 = "Saturday";
-  var day7 = "Sunday";
-
   var d = new Date();
-  var n = d.getDay();
-  document.write(dayn)
+  var weekday = new Array(7);
+  weekday[0]=  "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d.getDay()];
+  document.write(n);
 }
 
 dotw();
