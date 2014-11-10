97ca2d262654460fc05c3cd084973da034d0fd22
jakey1610 <mortimer907@gmail.com>
2014-11-10 17:43:59 +0000

    Timetable changes for a dynamic greeting

--- Timetable/main.js
+++ Timetable/main.js
@@ -11,9 +11,9 @@ function dotw(){
 
   var day = weekday[d.getDay()];
   document.getElementById("dotw").innerHTML = "Today is " + "<u>" + day + "</u>";
-  var timetable = ["You shouldn't be at school today..", "If it is Week 1 then : <br /> Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... . <br /> If it is Week 2 then you have: Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... .",
-  "If it is Week 1 then : <br /> Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... . <br /> If it is Week 2 then you have: Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... .", "If it is Week 1 then : <br /> Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... . <br /> If it is Week 2 then you have: Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... .",
-  "If it is Week 1 then : <br /> Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... . <br /> If it is Week 2 then you have: Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... .", "If it is Week 1 then : <br /> Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... . <br /> If it is Week 2 then you have: Firstly, you should have ... . <br /> Then, you have ... . <br /> After that you have ... . <br /> Just before lunch you have ... . <br /> Lastly you have ... .", "You shouldn't be at school today..."];
+  var timetable = ["You shouldn't be at school today..", "If it is Week 1 then : <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... . <br /><br /> If it is Week 2 then: <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... .",
+  "If it is Week 1 then : <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... . <br /><br /> If it is Week 2 then: <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... .", "If it is Week 1 then : <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... . <br /><br /> If it is Week 2 then: <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... .",
+  "If it is Week 1 then : <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... . <br /><br /> If it is Week 2 then: <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... .", "If it is Week 1 then : <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... . <br /><br /> If it is Week 2 then: <br /><br /> Firstly, you should have ... . <br /><br /> Then, you have ... . <br /><br /> After that you have ... . <br /><br /> Just before lunch you have ... . <br /><br /> Lastly you have ... .", "You shouldn't be at school today..."];
   var timetable = timetable[d.getDay()];
   document.getElementById("timetable").innerHTML = timetable;
 
@@ -28,6 +28,7 @@ function startTime() {
     s = checkTime(s);
     document.getElementById("time").innerHTML = "The current time in Great Britain is " + "<u>"+ h+":"+m+":"+s + "</u>" + ".";
     var t = setTimeout(function(){startTime()},500);
+
 }
 
 function checkTime(i) {
@@ -35,6 +36,21 @@ function checkTime(i) {
     return i;
 }
 
+function greet(){
+  var today=new Date();
+  var h=today.getHours();
+  var greeting;
+    if (h < 10) {
+        greeting = "Good morning, Mr. Mortimer";
+    } else if (h < 16) {
+        greeting = "Good day, Mr. Mortimer";
+    } else {
+        greeting = "Good evening, Mr. Mortimer";
+    }
+    document.getElementById("greeting").innerHTML = greeting;
+}
+
+greet();
 dotw();
 checkTime();
 startTime();
